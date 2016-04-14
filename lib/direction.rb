7a78fe3b957aefb083541ecef6294bb900a1b821
Kirk Smith <kirks@hotmail.co.uk>
2016-04-14 16:50:44 +0100

    turn left and right added to direction class

--- lib/direction.rb
+++ lib/direction.rb
@@ -5,4 +5,32 @@ class Direction
   def initialize(point)
     @point = point
   end
+
+  def turn_left
+    case
+    when @point == 'N'
+      @point = 'W'
+    when @point == 'W'
+      @point = 'S'
+    when @point == 'S'
+      @point = 'E'
+    when @point == 'E'
+      @point = 'N'
+    end
+  end
+
+  def turn_right
+    case
+    when @point == 'N'
+      @point = 'E'
+    when @point == 'E'
+      @point = 'S'
+    when @point == 'S'
+      @point = 'W'
+    when @point == 'W'
+      @point = 'N'
+    end
+
+  end
+
 end
--- spec/direction_spec.rb
+++ spec/direction_spec.rb
@@ -10,4 +10,61 @@ describe 'Direction' do
     expect(direction.point).to eq 'N'
   end
 
+  describe '#turn_left' do
+
+    it 'turn to face west when facing north' do
+      direction.turn_left
+      expect(direction.point).to eq 'W'
+    end
+
+    it 'turn to face south when facing east' do
+      direction.turn_left
+      direction.turn_left
+      expect(direction.point).to eq 'S'
+    end
+
+    it 'turn to face east when facing south' do
+      direction.turn_left
+      direction.turn_left
+      direction.turn_left
+      expect(direction.point).to eq 'E'
+    end
+
+    it 'turn to face north when facing east' do
+      direction.turn_left
+      direction.turn_left
+      direction.turn_left
+      direction.turn_left
+      expect(direction.point).to eq 'N'
+    end
+  end
+
+  describe '#turn_right' do
+
+    it 'turn to face east when facing north' do
+      direction.turn_right
+      expect(direction.point).to eq 'E'
+    end
+
+    it 'turn to face south when facing east' do
+      direction.turn_right
+      direction.turn_right
+      expect(direction.point).to eq 'S'
+    end
+
+    it 'turn to face west when facing south' do
+      direction.turn_right
+      direction.turn_right
+      direction.turn_right
+      expect(direction.point).to eq 'W'
+    end
+
+    it 'turn to face north when facing west' do
+      direction.turn_right
+      direction.turn_right
+      direction.turn_right
+      direction.turn_right
+      expect(direction.point).to eq 'N'
+    end
+  end
 end
